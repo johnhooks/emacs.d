9b5315f43b5b01b344f3b34185d4048ba76767ae
John Hooks <john@bitmachina.com>
2017-07-20 15:27:46 -0700

    Commit to Jove Mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (js2-mode golden-ratio paredit rainbow-delimiters whitespace-cleanup-mode flycheck yasnippet auto-complete projectile smex ido-vertical-mode flx-ido nvm exec-path-from-shell use-package))))
+    (web-mode htmlize js2-mode golden-ratio paredit rainbow-delimiters whitespace-cleanup-mode flycheck yasnippet auto-complete projectile smex ido-vertical-mode flx-ido nvm exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
